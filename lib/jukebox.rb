7a268cbe0bcd025f6d754d3bfe9eec9e309bce05
Hope Giometti <hopegiometti@gmail.com>
2019-10-04 18:47:51 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -53,6 +53,8 @@ def run(user_command)
       help
     elsif user_command == "list"
       list(songs)
+      puts "Please enter a command:"
+      input = gets.strip
     elsif user_command == "play"
       play(songs)
     else
