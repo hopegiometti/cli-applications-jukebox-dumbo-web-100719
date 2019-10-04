c6f27dca9e01f1e823140d6a29c833b4f53d6a7e
Hope Giometti <hopegiometti@gmail.com>
2019-10-04 17:57:05 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,24 @@
-# Add your code here
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+puts "Enter your name:"
+
+users_name = gets.strip
+
+def say_hello(name)
+  "Hi #{name}!"
+end
+
+
+
+
+puts say_hello(users_name)
